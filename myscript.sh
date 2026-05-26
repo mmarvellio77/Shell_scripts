1b736aa56835124ccaeef6c914cf1ff17c942934
marvellio77 <m.munezero@alustudent.com>
2026-05-26 10:29:03 +0200

    Add myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+Directory_Name="assignment_dir"
+Path="/Users/munezeromarvella/Shell_scripts"
+if [ -d "$Path/$Directory_Name" ]; then
+echo "Directory with a Name '$Directory_Name' exits in the Location"
+else
+echo "No such a Directory"
+fi
+
+
+
+
+

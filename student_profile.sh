dcffbf48c7cf78ff5175cc4a2265c26d6988e4e4
marvellio77 <m.munezero@alustudent.com>
2026-06-02 07:18:07 +0200

    Complete Activity 4 - create student profile script

---
+++ student_profile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+student_name="Marvella"
+cohort="Cohort 1"
+favorite_command="ls"
+echo "Student Name: $student_name"
+echo "Cohort: $cohort"
+echo "Favourite Linux command: $favorite_command"
+
